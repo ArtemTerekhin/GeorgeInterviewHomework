697573d154e0923df1c90c94b8ed1d1281355999
Artem Terekhin <artem@byallmeans.work>
2025-05-16 22:37:19 +0200

    CreateProject

---
+++ GeorgeInterviewHomework/GeorgeInterviewHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeorgeInterviewHomeworkApp.swift
+//  GeorgeInterviewHomework
+//
+//  Created by Artem Terekhin on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GeorgeInterviewHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeorgeInterviewHomeworkTests/GeorgeInterviewHomeworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  GeorgeInterviewHomeworkTests.swift
+//  GeorgeInterviewHomeworkTests
+//
+//  Created by Artem Terekhin on 16.05.2025.
+//
+
+import Testing
+@testable import GeorgeInterviewHomework
+
+struct GeorgeInterviewHomeworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
